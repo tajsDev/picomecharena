3a94b3255cef655854429e7cd37a1e290abe8983
tajsDev <tomasjauregui14@gmail.com>
2024-08-07 01:39:34 -0700

    background compeleted now main game loop

--- src/background.zig
+++ src/background.zig
@@ -1,10 +1,20 @@
 const rl = @import("raylib");
 
-pub fn new() rl.Texture2D {
-    var startImage = rl.loadImage("images/download.png");
-    rl.imageResize(&startImage,160,160);
-    defer rl.unloadImage(startImage);
-    const startText = rl.loadTextureFromImage(startImage);
-    return startText;
-}
+
+pub const Background = struct {
+    texture: rl.Texture2D,
+    pub fn new() Background {
+        const startImage = rl.loadImage("images/background.png");
+        defer rl.unloadImage(startImage);
+        const startText = rl.loadTextureFromImage(startImage);
+        return Background {
+            .texture = startText
+        };
+    }
+
+    pub fn process(backg : Background) !void  {
+        rl.drawTexture(backg.texture,0,0,rl.Color.white);
+    }
+};
+
 
--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const rl = @import("raylib");
 const Game = @import("game.zig");
 const Player = @import("player.zig").Player;
 const control = @import("controls.zig");
+const back = @import("background.zig").Background;
 
 pub fn main() anyerror!void {
     const screenWidth = 1600;
@@ -13,7 +14,9 @@ pub fn main() anyerror!void {
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     var srtGame = Game.init();
     var cPlay = Player.new();
+    const cBack = back.new();
     defer rl.unloadTexture(cPlay.texture);
+    defer rl.unloadTexture(cBack.texture);
     srtGame.currentStatus = Game.GameStatus.playing;
     
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
@@ -21,6 +24,9 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
         cPlay.x = control.controlX(cPlay.x);
         cPlay.y = control.controlY(cPlay.y);
+        //set background 
+        try cBack.process();
+        //set player
         try cPlay.process();
         rl.clearBackground(rl.Color.black);
     }
