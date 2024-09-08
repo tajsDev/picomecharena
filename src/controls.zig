2a469f3d1edae395ce903f49b1a20b6e17345e62
tajsDev <tomasjauregui14@gmail.com>
2024-09-08 16:02:06 -0700

    more for zig

--- src/controls.zig
+++ src/controls.zig
@@ -17,14 +17,19 @@ pub fn controlInput(x:i32,y:i32) Pos {
     var newY:i32 = 0;
     var currentSpeed:i32 = speed;
     var result = Pos.new();
+
     //right
-    if (rl.isKeyDown(rl.KeyboardKey.key_right) or rl.isKeyDown(rl.KeyboardKey.key_d)) newX = 1;
+    if (rl.isKeyDown(rl.KeyboardKey.key_right) or 
+    rl.isKeyDown(rl.KeyboardKey.key_d)) newX = 1;
     //left
-    if (rl.isKeyDown(rl.KeyboardKey.key_left) or rl.isKeyDown(rl.KeyboardKey.key_a)) newX  = -1;
+    if (rl.isKeyDown(rl.KeyboardKey.key_left) or 
+    rl.isKeyDown(rl.KeyboardKey.key_a)) newX  = -1;
     //down
-    if (rl.isKeyDown(rl.KeyboardKey.key_up) or rl.isKeyDown(rl.KeyboardKey.key_w)) newY = -1;
+    if (rl.isKeyDown(rl.KeyboardKey.key_up) or 
+    rl.isKeyDown(rl.KeyboardKey.key_w)) newY = -1;
     //up
-    if (rl.isKeyDown(rl.KeyboardKey.key_down) or rl.isKeyDown(rl.KeyboardKey.key_s)) newY = 1;
+    if (rl.isKeyDown(rl.KeyboardKey.key_down) 
+    or rl.isKeyDown(rl.KeyboardKey.key_s)) newY = 1;
     //calcuate diag movement so not to go fast
     if(newX != 0 and newY != 0 ) {
         var aVal:f32 = @floatFromInt((speed * speed)/2);
@@ -33,8 +38,11 @@ pub fn controlInput(x:i32,y:i32) Pos {
 
     }
     const currX = x + (currentSpeed * newX);
+
     const currY = y + (currentSpeed * newY);
+
     result.xSpeed = currX;
     result.ySpeed = currY;
+
     return result;
 }
